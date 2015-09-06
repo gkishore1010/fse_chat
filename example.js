ce9f203d4a84f0ffd12eee36f40823759a36ea40
Gayatri Kishore <gayatri.kishore@gmail.com>
2015-09-06 14:01:45 -0700

    Final Stuff

--- example.js
+++ example.js
@@ -2,13 +2,14 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-//test code
 
+//to load files from the public folder
 app.use(express.static(__dirname + '/public'));
 
-//db code
+//DB setup code
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./chat.db');
+
 //create tables
 db.serialize(function(){
 	db.run("CREATE TABLE IF NOT EXISTS chats_table (name VARCHAR(30), time TEXT, message TEXT)");
@@ -17,23 +18,27 @@ db.serialize(function(){
 
 io.on('connection', function(client){
 
-	console.log('Client connected...');
+	
 	 
 	 client.on('join', function(name){
 	 	client.username = name;
+	 	console.log(name + ' connected...');
 
+	 	//insert client into table of active usernames
 	 	db.serialize(function(){	
 	 		var stmt = db.prepare("INSERT INTO users_table VALUES (?)");
 	 		stmt.run(name);
 	 		stmt.finalize();
 		});
-		
+
+		//load existing chat messages onto client
 	 	db.each("SELECT rowid as id, name, time, message FROM chats_table", function(err,row){
 	 		var chatMsg = row.name+"@"+row.time+","+row.message;
 	 		client.emit("messages", chatMsg);
 		});
 	});
 	
+	 //get chat messages and add to database
 	 client.on('messages', function(chatMessage){
 		console.log(chatMessage);
 		client.broadcast.emit('messages', chatMessage);				
@@ -52,23 +57,25 @@ io.on('connection', function(client){
 		
 	});
 
+	//remove user from table of active usernames
 	client.on('leave_chat', function (username){
 
-		// db.run("DELETE FROM users_table WHERE name= ?", [username], function(err){
-		// 	if(err){
-		// 	console.log(err);
-		// 	}
-		// });
+		 db.run("DELETE FROM users_table WHERE name= ?", [username], function(err){
+		 	if(err){
+		 	console.log(err);
+		 	}
+		 });
 		console.log(username + " has left chatroom");
 		
 	});
 
+	
 	client.on('disconnect', function(){
 		console.log("client disconnected..");
 	});
 });	
 
-
+//load index.html on route /
 app.get('/', function (req, res) {
  res.sendFile(__dirname + '/index.html');
 });
--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 			var socket = io.connect('http://localhost:8080');
 			var username;
-
+//function to extract timestamp
 			function getTimer(){
 				var d = new Date();
 				var hr = d.getHours();
@@ -36,9 +36,7 @@
 
 				var new_chat_message = document.createElement('div');
 				new_chat_message.className='message';
-				var new_message = document.createElement('p');
-				new_message.innerHTML=message;
-				new_chat_message.appendChild(new_message);
+				new_chat_message.innerHTML=message;
 
 				new_chat.appendChild(new_chat_user);
 				new_chat.appendChild(new_chat_timestamp);
@@ -50,53 +48,67 @@
 
 			}
 
+			function createHTMLChat(chatMessage){
+				var index = chatMessage.indexOf("@");
+				var username = chatMessage.substring(0, index);
+				var index2 = chatMessage.indexOf(",");
+				var timeStamp = chatMessage.substring(index+1, index2);
+				var message= chatMessage.substring(index2+1);
+				var console=document.getElementById('chat_console');
 
-			 document.getElementById('entry_form').onsubmit = function(e) {
+				var new_chat = document.createElement('li');
+			}
+
+
+			 document.getElementById('submit_button').onclick = function(e) {
 			 	e.preventDefault();
 			 	username = document.getElementById('username').value;
 			 	if(!username){
 			 		alert("Please enter username");
 			 		return false;
+			 	}
+			 	if(username.indexOf(',') > -1 || username.indexOf('@') > -1){
+			 		alert("Invalid character in username! Please re-enter username");
+			 		return false;	
 			 	}	
 				socket.emit('join', username);
 				document.getElementById('username').value = null;
+
+				$("#leaveChat").show();
+				$("#submit_button").hide();
+				$("#username").hide();
+				$("#chatRoom").show();
 			};
 
-			 document.getElementById('chat_form').onsubmit = function(e) {
+			 document.getElementById('submitChat').onclick = function(e) {
+				
 				e.preventDefault();
 				var time = getTimer();
 				var message = document.getElementById('chat_input').value;
 				var chatMessage = username + "@" + time + "," + message;
 				socket.emit('messages', chatMessage);
 				createHTML(chatMessage);
-				// var new_message = document.createElement('span');
-				// new_message.innerHTML = chatMessage+'<br/>';
-				// var console = document.getElementById('chat_console');
-				// console.appendChild(new_message);
-				
 				chat_input.value=null;
 
 			};
 
-			// socket.on('connect', function(data){
-			// 	alert("Hello!");
 
-			// });
 			socket.on('messages', function(chatMessage){
 
-			// var new_message = document.createElement('span');
-			// new_message.innerHTML = chatMessage + '<br/>';
-			// var console = document.getElementById('chat_console');
-			// console.appendChild(new_message);
-			// document.getElementById("chat_console").scrollTop= document.getElementById("chat_console").scrollHeight;
 			createHTML(chatMessage);
 			
 			 });
 
-			document.getElementById("leave_chat").onclick = function(e) {
+			document.getElementById("leaveChat").onclick = function(e) {
+				
 				e.preventDefault();
-				alert("hello there")
 				socket.emit('leave_chat', username);
 				document.getElementById("chat_console").innerHTML ="";
 				socket.username=null;
+
+				$("#chatRoom").hide();
+				$("#username").show();
+				$("#submit_button").show();
+				$("#leaveChat").hide();
+				$("#username").focus();
 			};
